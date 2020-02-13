33b147f278e7a9acb4d39fb6fd3d88f1d01a81e1
adzfam <43882268+adzfam@users.noreply.github.com>
2020-02-13 16:15:41 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+import names
+f = names.get_first_name()
+#print(f)
+l = names.get_last_name()
+#print(l)
+#print(f, l)
+#----------------------
+#dictionary = {
+ #   'First Name': f,  
+  #  'Last Name': l,     
+#}
+#print(dictionary)
+#------------------------
+#dicts = {}
+#keys = range(2)
+values = [f,l]
+#for i in keys:
+#    dicts[i] = values[i]
+#print(dicts)
+#------------------------
+#o = dict(list(enumerate(values)))
+#print(o)
+#for keys in dicts():
+ #   print(keys)
+#for values in dicts():
+ #   print(values)
+ d = {first name: (f), 'last name', l}
+ print(d)
