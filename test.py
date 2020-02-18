07404bf7729e2d5f621cfb3d097707900b0dcfa6
adzfam <43882268+adzfam@users.noreply.github.com>
2020-02-18 13:49:18 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,19 @@
+from random import random
+import uuid
 import names
 f = names.get_first_name()
 #print(f)
 l = names.get_last_name()
-#print(l)
-#print(f, l)
-#----------------------
-#dictionary = {
- #   'First Name': f,  
-  #  'Last Name': l,     
-#}
-#print(dictionary)
-#------------------------
-#dicts = {}
-#keys = range(2)
-values = [f,l]
-#for i in keys:
-#    dicts[i] = values[i]
-#print(dicts)
-#------------------------
-#o = dict(list(enumerate(values)))
-#print(o)
-#for keys in dicts():
- #   print(keys)
-#for values in dicts():
- #   print(values)
- d = {first name: (f), 'last name', l}
- print(d)
+v = {}
+for x in range(5):
+ f = names.get_first_name()
+ l = names.get_last_name()
+ a = random()
+
+ v[uuid.uuid1()] = {"firstname":f, "lastname":l,"age": a}
+print(v)
+
+columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in v.keys())
+values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in v.values())
+sql = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('mytable', columns, values)
+print(sql)
