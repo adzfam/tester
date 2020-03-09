fb18f7e1440d6a4b8e7e2d2ec7a21aacd2730777
adzfam <adam.idrissi@goodboost.org>
2020-03-09 23:56:30 +0000

    User Transactions Code

--- User_Transactions.py
+++ User_Transactions.py
@@ -4,7 +4,6 @@ import names
 from datetime import datetime
 from faker import Faker
 
-   
 f = names.get_first_name()
 l = names.get_last_name()
 fake = Faker()
@@ -14,35 +13,50 @@ timestampStr = dt.strftime("%d-%b-%Y")
 
 ##Code for user accounts--------------------------------
 for _ in range(10):
- f = names.get_first_name()
- l = names.get_last_name()
- age = random.randint(5,20)
- address = fake.address()
+    f = names.get_first_name()
+    l = names.get_last_name()
+    age = random.randint(5, 20)
+    address = fake.address()
 
- v[uuid.uuid1()] = {"firstname":f,
-                    "lastname":l,
-                    "age": age,
-                    "address": address,
-                    "date": timestampStr}
+    v[uuid.uuid1()] = {"firstname": f,
+                       "lastname": l,
+                       "age": age,
+                       "date": timestampStr}
 
-#prints with the key
-#for x in v.items():
- #print(x)
+# prints with the key
+# for x in v.items():
+# print(x)
 
-#prints without the key
+# prints without the key
 for x in v:
     print(v[x])
 
-#--------------------------------------------------------
-#Code for transactions & Money
-transactions = {"firstname":f,
-                "lastname":l,
+# --------------------------------------------------------
+# Code for transactions & Money
+# Inventory for purchases
+inventory = {"apple": 10,
+             "banana": 20,
+             "water": 50,
+             "soap": 30,
+             }
+trans - []
+total = []
+
+
+
+def transaction():
+
+
+
+
+transactions = {"firstname": f,
+                "lastname": l,
                 "balance": 100,
-}
+                }
 
 for x in transactions:
     print(transactions)
-#-----------------------------------------
+# -----------------------------------------
 # class People:
 #     pass
 # Person1 = People()
@@ -53,55 +67,57 @@ for x in transactions:
 # Person1.age = age
 # Person1.address = address
 # Person1.Money = 100
-#----------------
-class People:
-    def __init__(self, first, last, age, money):
-        self.first = first
-        self.last = last
-        self.age = age
-        self.money = money
-        self.index = 0
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.index >= len(self.):
-            raise StopIteration
-
-
-
-Person1 = People(f, l, age, 100)
-Person2 = People()
-
-print(People)
-#------------------------------------------
-# conn = sqlite3.connect('tester.db')
-# c = conn.cursor()
-# #create table
-# c.execute('''CREATE TABLE Tester
-#             (ID, first name, last name, age)''')
-# #insert row of data
-# c.execute("INSERT INTO Tester VALUES (v)")
+# #----------------
+# class People:
+#     def __init__(self, first, last, age, money):
+#         self.first = first
+#         self.last = last
+#         self.age = age
+#         self.money = money
+#         self.index = 0
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.index >= len(self.):
+#             raise StopIteration
 #
 #
-# #Save commit changes
-# conn.commit()
 #
-# #close after saving
-# c.close()
-# conn.close()
+# Person1 = People(f, l, age, 100)
+# Person2 = People()
 #
-# for row in c.execute('SELECT * FROM Tester ORDER BY first name'):
-#         print(row)
+# print(People)
+# #------------------------------------------
 #
-# #------------------------------------------------
+# #------------------------------------------
+# # conn = sqlite3.connect('tester.db')
+# # c = conn.cursor()
+# # #create table
+# # c.execute('''CREATE TABLE Tester
+# #             (ID, first name, last name, age)''')
+# # #insert row of data
+# # c.execute("INSERT INTO Tester VALUES (v)")
+# #
+# #
+# # #Save commit changes
+# # conn.commit()
+# #
+# # #close after saving
+# # c.close()
+# # conn.close()
+# #
+# # for row in c.execute('SELECT * FROM Tester ORDER BY first name'):
+# #         print(row)
+# #
+# # #------------------------------------------------
+# #
+# #
+# # # columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in v.keys())
+# # # values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in v.values())
+# # # sql = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('mytable', columns, values)
+# # # print(sql)
 #
 #
-# # columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in v.keys())
-# # values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in v.values())
-# # sql = "INSERT INTO %s ( %s ) VALUES ( %s );" % ('mytable', columns, values)
-# # print(sql)
-
-
-
+#
