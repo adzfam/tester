055d96ba1e6f30e086eb5ca870c2cc1def7c0819
adzfam <adam.idrissi@goodboost.org>
2020-03-10 03:10:33 +0000

    User Transactions Code

--- Transactions2.py
+++ Transactions2.py
@@ -10,19 +10,22 @@ dt = datetime.now()
 timestampStr = dt.strftime("%d-%b-%Y")
 # ----------------------------------------
 # List containing all the users
+keys = ["ID", "first name", "last name", "age", "time"]
+# Creating the Dict with UUID
 for _ in range(5):
     f = names.get_first_name()
     l = names.get_last_name()
     age = random.randint(9, 50)
     t = timestampStr
-    id = random.sample(range(1,5),1)
-    users = [f, l, age, t, id]
-    print(users)
+    id = uuid.uuid1()
+    users = [id, f, l, age, t]
+    new = {}
+    for i in range(5):
+        new[keys[i]] = users
+        print(new)
+print(new)
 # ---------------------------------------
-# Creating the Dict with UUID
-main = {uuid.uuid1(): {"firstname": f,
-                       "lastname": l,
-                       "age": age,
-                       "date": timestampStr}}
-# for x in users:
-#     main[x] =
+#new ] {" "}
+# main = dict(zip(keys, users))
+# print(main)
+
